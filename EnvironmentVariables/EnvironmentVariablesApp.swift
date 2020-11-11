4571d5d2857e1e2d5349408f18da1dbe0c4575e9
Max <maxim.macari.dam@gmail.com>
2020-11-11 18:41:56 +0100

    Initial Commit

---
+++ EnvironmentVariables/EnvironmentVariablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentVariablesApp.swift
+//  EnvironmentVariables
+//
+//  Created by Maxim Macari on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentVariablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
